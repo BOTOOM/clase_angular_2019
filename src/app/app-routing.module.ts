01155f6af33eb136c14d38aded7fa1d56862e043
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-10-07 21:42:42 -0500

    adicion de ruta de miembros y de su json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PruebaComponent } from './pages/prueba/prueba.component';
+import { MiembrosComponent } from './pages/miembros/miembros.component';
 
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'prueba', component: PruebaComponent },
+    { path: 'miembros', component: MiembrosComponent },
   //   { path: 'artist/:id', component: ArtistaComponent },
     { path: '', pathMatch: 'full', redirectTo: 'home' },	
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
